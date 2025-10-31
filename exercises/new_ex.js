59d2540f21c039599120e182cb9a8a7934c27265
Tori C <vika.cheklueva@gmail.com>
2025-10-30 18:16:26 -0700

    clicker

--- exercises/new_ex.js
+++ exercises/new_ex.js
@@ -1,15 +1,60 @@
-let count = 0;
-let colorIndex = 0;
-let mood;
-let colors = ["Orchid", "Coral", "HotPink", "Plum"]; 
+var count = 0;
+let colorCount = 0;
+var colors = ["Orchid", "Coral", "HotPink", "Plum"];
 
-$("#needy-button").click(function () { 
-    if (count < 5 ) { mood = "fresh and happy"; }
-    else if ((count >= 5 ) && (count < 10 )) { mood = "keep pushing"; }
-    else { mood = "so tired!"; }
-   $("#needy-button").html( "Clicks: " + count + " Color: " + colors[colorIndex] + " - " + mood);
-   $("body").css("background-color", colors[colorIndex]);
-   count = count + 1;
-   colorIndex = colorIndex + 1;
-   if (colorIndex == 4) [colorIndex = 0]
+
+function makeImage (imageName) {
+
+ if (colors[colorCount] == imageName) {
+       $("body").append("<img width=50 src='images/" + imageName + ".png'>");
+   }
+
+}
+
+function changeBackground (newColor) {
+   $("body").css("background-color", newColor);
+ }
+
+$(".color-button").click( function () {
+ 
+   changeBackground(this.id);
+
 });
+
+
+function moody (moodyCount){
+   let mood="";
+   if (moodyCount < 5) { mood = "gresh and happy"; }
+   else if ((moodyCount >= 5) && (moodyCount < 10)) { mood = "keep pushing"; }
+   else { mood = "so tired"; }
+
+   return mood;
+}
+
+// the button part
+$("#needy-button").click(function () {
+
+// move the mood deciding code outside as a seperate function and then call it from here, and use the result of the function for the button message
+   let moodMessage=moody(count);
+
+   $("#needy-button").html("Clicks: " + count + " Color: " + colors[colorCount] + " " + moodMessage);
+
+   changeBackground( colors[colorCount] );
+
+   makeImage("Coral"); 
+   makeImage("Orchid"); 
+   makeImage("Plum"); 
+
+
+   count = count + 1;
+   colorCount = colorCount + 1;
+   if (colorCount == 4) { colorCount = 0; }
+});
+
+$("#reset-button").click(function () {
+    count = 0;
+    $("#needy-button").html("click me");
+    $("body").css("background-color", "white")
+});
+
+
